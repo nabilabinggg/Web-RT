1a233a1e04f319341600a8127b15c026e15209e0
natamasa <pratamanayaka02@outlook.com>
2023-01-25 16:02:33 +0700

    perubahan home page

---
+++ app/Views/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website RT | RW</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="boxicons-2.1.4/css/boxicons.min.css">
+</head>
+<body>
+
+    <div class="showcase">
+
+        <!-- NAVBAR TOP SECTION -->
+        <div class="navbar-top">
+            <ul class="left">
+                <li><a href="#">Depok</a></li>
+                <li><a href="#"><i class="bx bxs-time bx-xs"></i>Senin - Sabtu: 08:00 - 15:00</a></li>
+                <li><a href="#"><i class="bx bxs-map bx-xs"></i>Depok, Jawa barat</a></li>
+            </ul>
+            <ul class="right">
+                <li><a href="#"><i class="bx bxl-instagram bx-xs"></i></a></li>
+                <li><a href="#"><i class="bx bxl-linkedin bx-xs"></i></a></li>
+                <li><a href="#"><i class="bx bxl-gmail bx-xs"></i></a></li>
+            </ul>
+        </div>
+
+        <!-- NAVBAR BOTTOM SECTION -->
+        <div class="navbar-bottom">
+            <a href="#" class="brand-left">NAMA WEB?</a>
+            <ul class="menu-right">
+                <li><a href="index.html">Beranda</a></li>
+                <li><a href="#">Tentang</a></li>
+                <li><a href="#">Kontak</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#"><i class="bx bx-search bx-sm" id="search-btn"></i></a></li>
+            </ul>
+            <div class="search-form">
+                <input type="search" name="search-box" id="search-box" placeholder="Masukkan pencarian.."
+                >
+                <label for="search-box" class="bx bx-search bx-sm"></label>
+            </div>
+        </div>
+
+        <!-- SHOWCASE CONTENT SECTION -->
+        <div class="showcase-content">
+            <h1>JUDUL WEB? <br> Gatau apa <span>nantinya</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Magni sed voluptas accusantium iure nisi placeat in nemo</p>
+            <a href="#" class="btn btn-left">EXPLORE MORE</a>
+            <a href="#" class="btn btn-right">DUMMY</a>
+        </div>
+    </div>
+
+        <!-- FITUR SECTION -->
+        <section id="fitur" class="bg-light py-3">
+            <div class="wrapper">
+                <div class="items">
+                    <div class="item">
+                        <i class='bx bx-male-female bx-lg'></i>
+                        <h3>Administrasi Warga</h3>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                    </div>
+                    <div class="item">
+                        <i class='bx bxs-home-alt-2 bx-lg'></i>
+                        <h3>Rukun Tetangga</h3>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                    </div>
+                    <div class="item">
+                        <i class='bx bxs-building-house bx-lg'></i>
+                        <h3>Rukun Warga</h3>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                    </div>
+                    <div class="item">
+                        <i class='bx bxs-check-shield bx-lg'></i>
+                        <h3>DKM</h3>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam vitae saepe quia voluptate</li>
+                    </div>
+                </div>
+                <div id="tentang" class="tentang">
+                    <div class="tentang-img">
+                        <img src="/img/bgtentang.jpg">
+                    </div>
+                    <div class="tentang-text">
+                        <h2>Tentang Kami</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <div class="contents">
+                            <div class="content p-4">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script src="app.js"></script>
+</body>
+</html>
