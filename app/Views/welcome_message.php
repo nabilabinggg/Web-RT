1f0787cccd82e68d75562156416f4b8811d618e1
fauzaqiilah <sqiqilah@gmail.com>
2023-01-25 16:20:15 +0700

    perubahan coba

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 5!</title>
+    <title>Welcome to CodeIgniter 7!</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
