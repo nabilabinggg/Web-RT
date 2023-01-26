ea8c28456658e77932c6fad16ada0960c1202918
fauzaqiilah <sqiqilah@gmail.com>
2023-01-26 22:24:45 +0700

    perubahan home1

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,13 +6,10 @@ class Home extends BaseController
 {
     public function index_()
     {
-<<<<<<<<< Temporary merge branch 1
         $data = [
             'title' => 'RT'
         ];
         return view('pages/homepage', $data);
-=========
-        return view('welcome_message');
->>>>>>>>> Temporary merge branch 2
+
     }
 }
