86f3d195573b74976de88e79098274049c526761
natamasa <pratamanayaka02@outlook.com>
2023-01-28 08:02:50 +0700

    ubah home page

--- app/Views/pages/homepage.php
+++ app/Views/pages/homepage.php
@@ -3,85 +3,83 @@
 
 <!-- Css -->
 <link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <section class="homepage">
     <!-- Header -->
     <div class="container">
         <div class="banner">
             <div class="banner-text">
-                <h1>Judul Web</h1>
-                <button>Explore More</button>
+                <h1>SIDUBI</h1>
+                <p>SIDUBI merupakan website khusus RT 001 - 009 Desa LPUG Kecamatan LPUG
+                <br>Kota Depok Provinsi Jawa Barat<br></p>
+                <button>Lebih Lanjut</button>
             </div>
-        </div>
+        </div><br>
 
-        <div class="cards">
-            <div class="card">
-                <div class="content">
-                    <div>
-                        <img src="<?php echo base_url('img/Vector.svg') ?>" alt="vector" style="height:60px; width:50px; object-fit:contain;">
-                        <h2>Administrasi warga</h2>
-                    </div>
-                    <ul>
-                        <li>Tentang</li>
-                        <li>Kontak</li>
-                        <li>Beranda</li>
-                        <li>Login</li>
-                    </ul>
-                </div>
+        <!-- DATA WARGA SECTION -->
+            <div class="tabel">
+                <h1><center>Data Warga</center></h1><br>
+                <table class="table">
+                    <thead>
+                        <th>RT</th>
+                        <th>Penyewa</th>
+                        <th>Tetap</th>
+                        <th>Balita</th>
+                        <th>Remaja</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>001</td>
+                            <td>10</td>
+                            <td>14</td>
+                            <td>5</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>002</td>
+                            <td>5</td>
+                            <td>3</td>
+                            <td>0</td>
+                            <td>12</td>
+                        </tr>
+                        <tr>
+                            <td>003</td>
+                            <td>14</td>
+                            <td>2</td>
+                            <td>4</td>
+                            <td>11</td>
+                        </tr>
+                        <tr>
+                            <td>004</td>
+                            <td>8</td>
+                            <td>12</td>
+                            <td>3</td>
+                            <td>9</td>
+                        </tr>
+                        <tr>
+                            <td>005</td>
+                            <td>23</td>
+                            <td>1</td>
+                            <td>4</td>
+                            <td>7</td>
+                        </tr>
+                    </tbody>
+                </table><br>
+            </div>
+            <div class="button">
+                <a href="#" class="btn btn-left"> < Sebelumnya</a>
+                <a href="#" class="btn btn-right">Selanjutnya ></a>
             </div>
 
-            <div class="card">
-                <div class="content">
-                    <div>
-                        <img src="<?php echo base_url('img/Vector.svg') ?>" alt="vector" style="height:60px; width:50px; object-fit:contain;">
-                        <h2>Administrasi warga</h2>
-                    </div>
-                    <ul>
-                        <li>Tentang</li>
-                        <li>Kontak</li>
-                        <li>Beranda</li>
-                        <li>Login</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="content">
-                    <div>
-                        <img src="<?php echo base_url('img/Vector.svg') ?>" alt="vector" style="height:60px; width:50px; object-fit:contain;">
-                        <h2>Administrasi warga</h2>
-                    </div>
-                    <ul>
-                        <li>Tentang</li>
-                        <li>Kontak</li>
-                        <li>Beranda</li>
-                        <li>Login</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="content">
-                    <div>
-                        <img src="<?php echo base_url('img/Vector.svg') ?>" alt="vector" style="height:60px; width:50px; object-fit:contain;">
-                        <h2>Administrasi warga</h2>
-                    </div>
-                    <ul>
-                        <li>Tentang</li>
-                        <li>Kontak</li>
-                        <li>Beranda</li>
-                        <li>Login</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
+        <!-- TENTANG SECTION -->
         <div class="about">
             <div class="about-content">
                 <img src="<?php echo base_url('img/homepage.png') ?>" alt="about" style="width:30%; object-fit:contain; background-color:transparent; margin:60px;">
                 <div class="about-content-text">
                     <h2>Tentang Kami</h2>
-                    <p>ini adalah tentang kami........</p>
+                    <p>SIDUBI merupakan website khusus RT 001 - 009 Desa LPUG Kecamatan LPUG Kota Depok Jawa Barat</p>
                 </div>
             </div>
         </div>
