7d8c6abb01f50b42766d52b4a289116f541aaae5
Your Name <you@example.com>
2023-02-01 15:31:24 +0700

    UPDATE LOGIN

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,6 +15,7 @@ $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
+
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
@@ -33,6 +34,7 @@ $routes->get('/', 'Home::index');
 $routes->get('/login', 'Login::index', ['as' => 'login']);
 $routes->post('/login', 'Login::login',);
 $routes->get('/homepage', 'auth::homepage',);
+$routes->get('/dashboard', 'dashboard::index',);
 
 
 
--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -19,30 +19,28 @@
         <!-- Header -->
         <div class="container">
             <div style="display: flex;">
-                <img src="<?php echo base_url('img/homepage.png') ?>" width="50%" height="100%"
-                    style="position: absolute; left: 0;">
-                <div
-                    style="display: flex; flex-direction:column; padding: 50px; width: 50%; position: absolute; right:0; justify-content:space-between;">
+                <img src="<?php echo base_url('img/homepage.png') ?>" width="50%" height="100%" style="position: absolute; left: 0; transform:rotate()">
+                <div style="display: flex; flex-direction:column; padding: 50px; width: 50%; position: absolute; right:0; justify-content:space-between;">
                     <div>
-                        <h1 style="text-align: center; font-size: 40px; font-family:'Inika'; font-weight: 400;">Nama Web
-                        </h1>
+                        <h1 style="text-align: center; font-size: 40px; font-family:'Arial'; font-weight: 400;"><b>SIDUBI
+
+                            </b></h1>
                     </div>
                     <form style="display: flex; flex-direction:column; margin-top: 20%">
                         <div style="margin-bottom: 40px; height: 81px;">
                             <i class="far fa-user-circle" style="font-size: 50px; margin-right:20px;"></i>
-                            <input
-                                style="border: 3px solid #020000; border-radius: 30px; padding: 20px; font-size: large; width:90%; float:right;"
-                                name="username" placeholder="Username">
+                            <input style="border: 3px solid #020000; border-radius: 20px; padding: 20px; font-size: large; width:90%; float:right;" name="username" placeholder="Username">
                         </div>
                         <div style="margin-bottom: 30px; height: 81px;">
+                            <!-- <i class="fa-regular fa-circle style= font-size: 50px; margin-right:20px;"></i> -->
                             <i class="fas fa-lock" style="font-size: 50px; margin-right:20px;"></i>
-                            <input
-                                style="border: 3px solid #020000; border-radius: 30px; padding: 20px; font-size: large; width:90%; float:right;"
-                                name="password" placeholder="password">
+                            <input style="border: 3px solid #020000; border-radius: 20px; padding: 20px; font-size: large; width:90%; float:right;" name="password" placeholder="Password">
                         </div>
-                        <a style="margin:0 0 30px auto; font-size:20px;">Forgot Password?</a>
-                        <button
-                            style="height: 64px; width:auto; background: #84D9BB; border: 3px solid #000000; border-radius: 20px; <?php echo ("pages/homepage.php") ?>">Login</button>
+                        <br>
+                        <br>
+                        <br>
+                        <!-- <a href="#" style="margin:0 0 30px auto; font-size:20px; color:blue;"><u>Forgot Password?</u></a> -->
+                        <button style="margin-left:75px; height: 64px; width:auto; background: #84D9BB;  border-radius: 20px; <?php echo ("pages/homepage.php") ?>">Login</button>
                     </form>
                 </div>
             </div>
